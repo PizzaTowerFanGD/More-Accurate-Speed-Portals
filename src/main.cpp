36d4011e53dfd8e21a320dfd08f644b3b7d97fea
MarioMastr <ashrmoha11@gmail.com>
2025-01-04 12:57:32 +0000

    - add more explanations as to what the hell i'm doing - change the names of the classes to be more aligned with the point of the mod instead of just "my" - add name to EffectGameObject because why not - load all the settings in the portalSpeeds(Speed speed) function instead of outside all functions because it removes the requirement to restart the game every time you change the speed - change return type of portalspeeds function to float from double because in hindsight it was pissing me off that it was different

--- src/main.cpp
+++ src/main.cpp
@@ -9,38 +9,43 @@ using namespace geode::prelude;
 // for the randomised numbers
 #include <random>
 
-// Load the settings outside of a function
-bool enabled     = Mod::get()->getSettingValue<bool>("enable-mod");
-bool randomSpeed = Mod::get()->getSettingValue<bool>("random-speed");
-
-float minSpeedSlow = static_cast<float>(Mod::get()->getSettingValue<double>("min-speed-slow"));
-float maxSpeedSlow = static_cast<float>(Mod::get()->getSettingValue<double>("max-speed-slow"));
-
-float minSpeedNormal = static_cast<float>(Mod::get()->getSettingValue<double>("min-speed-normal"));
-float maxSpeedNormal = static_cast<float>(Mod::get()->getSettingValue<double>("max-speed-normal"));
-
-float minSpeedFast = static_cast<float>(Mod::get()->getSettingValue<double>("min-speed-fast"));
-float maxSpeedFast = static_cast<float>(Mod::get()->getSettingValue<double>("max-speed-fast"));
-
-float minSpeedFaster = static_cast<float>(Mod::get()->getSettingValue<double>("min-speed-faster"));
-float maxSpeedFaster = static_cast<float>(Mod::get()->getSettingValue<double>("max-speed-faster"));
-
-float minSpeedFastest = static_cast<float>(Mod::get()->getSettingValue<double>("min-speed-fastest"));
-float maxSpeedFastest = static_cast<float>(Mod::get()->getSettingValue<double>("max-speed-fastest"));
-
-float halfSpeed      = static_cast<float>(Mod::get()->getSettingValue<double>("half-speed"));
-float fullSpeed      = static_cast<float>(Mod::get()->getSettingValue<double>("full-speed"));
-float doubleSpeed    = static_cast<float>(Mod::get()->getSettingValue<double>("double-speed"));
-float tripleSpeed    = static_cast<float>(Mod::get()->getSettingValue<double>("triple-speed"));
-float quadrupleSpeed = static_cast<float>(Mod::get()->getSettingValue<double>("quadruple-speed"));
-
+// randomised speed logic
 float speedRandomiser(float minSpeed, float maxSpeed) {
     static std::default_random_engine e;
     static std::uniform_real_distribution<float> dis(minSpeed, maxSpeed);
     return dis(e);
 }
 
-double portalSpeeds(Speed speed) {
+// assigning float values to Speed enum members
+float portalSpeeds(Speed speed) {
+    // Load the settings (originally we wanted to load the settings outside of the function but it was causing issues):
+    // bool settings
+    bool enabled     = Mod::get()->getSettingValue<bool>("enable-mod");
+    bool randomSpeed = Mod::get()->getSettingValue<bool>("random-speed");
+
+    // min and max speeds for randomisation
+    float minSpeedSlow = static_cast<float>(Mod::get()->getSettingValue<double>("min-speed-slow"));
+    float maxSpeedSlow = static_cast<float>(Mod::get()->getSettingValue<double>("max-speed-slow"));
+
+    float minSpeedNormal = static_cast<float>(Mod::get()->getSettingValue<double>("min-speed-normal"));
+    float maxSpeedNormal = static_cast<float>(Mod::get()->getSettingValue<double>("max-speed-normal"));
+
+    float minSpeedFast = static_cast<float>(Mod::get()->getSettingValue<double>("min-speed-fast"));
+    float maxSpeedFast = static_cast<float>(Mod::get()->getSettingValue<double>("max-speed-fast"));
+
+    float minSpeedFaster = static_cast<float>(Mod::get()->getSettingValue<double>("min-speed-faster"));
+    float maxSpeedFaster = static_cast<float>(Mod::get()->getSettingValue<double>("max-speed-faster"));
+
+    float minSpeedFastest = static_cast<float>(Mod::get()->getSettingValue<double>("min-speed-fastest"));
+    float maxSpeedFastest = static_cast<float>(Mod::get()->getSettingValue<double>("max-speed-fastest"));
+
+    // singular speed values for when randomisation is disabled
+    float halfSpeed      = static_cast<float>(Mod::get()->getSettingValue<double>("half-speed"));
+    float fullSpeed      = static_cast<float>(Mod::get()->getSettingValue<double>("full-speed"));
+    float doubleSpeed    = static_cast<float>(Mod::get()->getSettingValue<double>("double-speed"));
+    float tripleSpeed    = static_cast<float>(Mod::get()->getSettingValue<double>("triple-speed"));
+    float quadrupleSpeed = static_cast<float>(Mod::get()->getSettingValue<double>("quadruple-speed"));
+
     switch (speed) {
         case Speed::Slow: {
             if (enabled) {
@@ -50,7 +55,7 @@ double portalSpeeds(Speed speed) {
                     return speedRandomiser(minSpeedSlow, maxSpeedSlow);
             }
             else
-                return 0.7f;
+                return 0.7;
         }
         case Speed::Normal: {
             if (enabled) {
@@ -60,7 +65,7 @@ double portalSpeeds(Speed speed) {
                     return speedRandomiser(minSpeedNormal, maxSpeedNormal);
             }
             else
-                return 0.9f;
+                return 0.9;
         }
         case Speed::Fast: {
             if (enabled) {
@@ -70,7 +75,7 @@ double portalSpeeds(Speed speed) {
                     return speedRandomiser(minSpeedFast, maxSpeedFast);
             }
             else
-                return 1.1f;
+                return 1.1;
         }
         case Speed::Faster: {
             if (enabled) {
@@ -80,7 +85,7 @@ double portalSpeeds(Speed speed) {
                     return speedRandomiser(minSpeedFaster, maxSpeedFaster);
             }
             else
-                return 1.3f;
+                return 1.3;
         }
         case Speed::Fastest: {
             if (enabled) {
@@ -90,12 +95,14 @@ double portalSpeeds(Speed speed) {
                     return speedRandomiser(minSpeedFastest, maxSpeedFastest);
             }
             else
-                return 1.6f;
+                return 1.6;
         }
     }
 }
 
-class $modify(MyPlayer, PlayerObject) {
+// speed logic:
+// player
+class $modify(SpeedPlayer, PlayerObject) {
     void updateTimeMod(Speed speed, bool p1) {
         float playerSpeed = portalSpeeds(speed);
         PlayerObject::updateTimeMod(playerSpeed, p1);
@@ -103,13 +110,14 @@ class $modify(MyPlayer, PlayerObject) {
     }
 };
 
-class $modify(MyGJBGL, GJBaseGameLayer) {
-    void updateTimeMod(Speed speed, bool p1, bool p2) {
+// assigning speeds to portals when starting level (p0->m_startSpeed)
+class $modify(SpeedGJBGL, GJBaseGameLayer) {
+    void updateTimeMod(Speed speed, bool p1) {
         this->m_gameState.m_timeModRelated  = 0;
         this->m_gameState.m_timeModRelated2 = 0;
-        as<MyPlayer *>(this->m_player1)->updateTimeMod(speed, p2);
+        as<SpeedPlayer *>(this->m_player1)->updateTimeMod(speed, p1); 
         if (this->m_gameState.m_isDualMode)
-            as<MyPlayer *>(this->m_player2)->updateTimeMod(speed, p2);
+            as<SpeedPlayer *>(this->m_player2)->updateTimeMod(speed, p1);
     }
 
     void setupLevelStart(LevelSettingsObject* p0) {
@@ -117,52 +125,52 @@ class $modify(MyGJBGL, GJBaseGameLayer) {
 
         switch (p0->m_startSpeed) {
             case Speed::Slow: {
-                as<MyPlayer *>(this->m_player1)->updateTimeMod(Speed::Slow, 0);
+                as<SpeedPlayer *>(this->m_player1)->updateTimeMod(Speed::Slow, 0);
                 if (this->m_gameState.m_isDualMode)
-                    as<MyPlayer *>(this->m_player2)->updateTimeMod(Speed::Slow, 0);
+                    as<SpeedPlayer *>(this->m_player2)->updateTimeMod(Speed::Slow, 0);
                 break;
             }
             case Speed::Normal: {
-                as<MyPlayer *>(this->m_player1)->updateTimeMod(Speed::Normal, 0);
+                as<SpeedPlayer *>(this->m_player1)->updateTimeMod(Speed::Normal, 0);
                 if (this->m_gameState.m_isDualMode)
-                    as<MyPlayer *>(this->m_player2)->updateTimeMod(Speed::Normal, 0);
+                    as<SpeedPlayer *>(this->m_player2)->updateTimeMod(Speed::Normal, 0);
                 break;
             }
             case Speed::Fast: {
-                as<MyPlayer *>(this->m_player1)->updateTimeMod(Speed::Fast, 0);
+                as<SpeedPlayer *>(this->m_player1)->updateTimeMod(Speed::Fast, 0);
                 if (this->m_gameState.m_isDualMode)
-                    as<MyPlayer *>(this->m_player2)->updateTimeMod(Speed::Fast, 0);
+                    as<SpeedPlayer *>(this->m_player2)->updateTimeMod(Speed::Fast, 0);
                 break;
             }
             case Speed::Faster: {
-                as<MyPlayer *>(this->m_player1)->updateTimeMod(Speed::Faster, 0);
+                as<SpeedPlayer *>(this->m_player1)->updateTimeMod(Speed::Faster, 0);
                 if (this->m_gameState.m_isDualMode)
-                    as<MyPlayer *>(this->m_player2)->updateTimeMod(Speed::Faster, 0);
+                    as<SpeedPlayer *>(this->m_player2)->updateTimeMod(Speed::Faster, 0);
                 break;
             }
             case Speed::Fastest: {
-                as<MyPlayer *>(this->m_player1)->updateTimeMod(Speed::Fastest, 0);
+                as<SpeedPlayer *>(this->m_player1)->updateTimeMod(Speed::Fastest, 0);
                 if (this->m_gameState.m_isDualMode)
-                    as<MyPlayer *>(this->m_player2)->updateTimeMod(Speed::Fastest, 0);
+                    as<SpeedPlayer *>(this->m_player2)->updateTimeMod(Speed::Fastest, 0);
                 break;
             }
         }
     }
 };
 
-class $modify(EffectGameObject) {
+class $modify(SpeedEffectGameObject, EffectGameObject) {
     void triggerObject(GJBaseGameLayer* p0, int p1, gd::vector<int> const* p2) {
         EffectGameObject::triggerObject(p0, p1, p2);
 
         if (this->m_objectID == 200) // 0.5x portal
-            as<MyGJBGL *>(p0)->updateTimeMod(Speed::Slow, 0, this->m_hasNoEffects);
+            as<SpeedGJBGL *>(p0)->updateTimeMod(Speed::Slow, this->m_hasNoEffects);
         else if (this->m_objectID == 201) // 1x portal
-            as<MyGJBGL *>(p0)->updateTimeMod(Speed::Normal, 0, this->m_hasNoEffects);
+            as<SpeedGJBGL *>(p0)->updateTimeMod(Speed::Normal, this->m_hasNoEffects);
         else if (this->m_objectID == 202) // 2x portal
-            as<MyGJBGL *>(p0)->updateTimeMod(Speed::Fast, 0, this->m_hasNoEffects);
+            as<SpeedGJBGL *>(p0)->updateTimeMod(Speed::Fast, this->m_hasNoEffects);
         else if (this->m_objectID == 203) // 3x portal
-            as<MyGJBGL *>(p0)->updateTimeMod(Speed::Faster, 0, this->m_hasNoEffects);
+            as<SpeedGJBGL *>(p0)->updateTimeMod(Speed::Faster, this->m_hasNoEffects);
         else if (this->m_objectID == 1334) // 4x portal
-            as<MyGJBGL *>(p0)->updateTimeMod(Speed::Fastest, 0, this->m_hasNoEffects);
+            as<SpeedGJBGL *>(p0)->updateTimeMod(Speed::Fastest, this->m_hasNoEffects);
     }
 };
